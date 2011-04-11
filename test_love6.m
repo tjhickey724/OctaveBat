d399124fd7286fb066dd71876a204f230473adf8
tim <tjhickey@brandeis.edu>
2011-04-11 09:40:27 -0400

    added some doc lines

--- test_love6.m
+++ test_love6.m
@@ -1,5 +1,6 @@
 function e = test_love6()
 % test_love6 runs several tests and returns
+% love6(a,b) should be true if a or b is six or if the sum or difference is 6
 % the number of tests that fail
 % Author: Mehreen Asad
 % Homework#4 
