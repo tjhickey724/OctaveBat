17676ea7f1336c41963d786923835df5897c2905
Mehreen Asad <mehreenasad@Mehreen-Asads-MacBook-Pro.local>
2011-04-20 00:04:20 -0400

    Added test cases

--- test_makeChocolate.m
+++ test_makeChocolate.m
@@ -2,12 +2,13 @@ function test_makeChocolate()
 % test_makeChocolate() runs 12 tests on the function makeChocolate to check reliability
 % function returns total number of failures.
 % Taha Bakhtiyar 2/2011
+% Mehreen Asad (Added 3 test cases to see if the test code works correctly)
   e = 0;
   e = e + runtest(0,0,0,0);
   e = e + runtest(1,1,1,1);
   e = e + runtest(1,5,6,1);
   e = e + runtest(1,5,5,0);
-  e = e + runtest(1,2,3,-1);
+  e = e + runtest(1,2,3,1);
   e = e + runtest(2,3,1,1);
   e = e + runtest(3,2,1,1);
   e = e + runtest(-2,0,0,-1);
@@ -15,12 +16,18 @@ function test_makeChocolate()
   e = e + runtest(7,13,21,1);
   e = e + runtest(7,3,21,6);
   e = e + runtest(7,2,21,-1);
+  e = e + runtest(2,10,14,2);
+  e = e + runtest(0,1,0,-1);
+  e = e + runtest(8,7,20,5);
+
+
+
   printf ('Failures: %d\n',e)
 end
 
 function e=runtest(a,b,c,ans)
 % runtest(a,b,c,ans) calls function makeChocolate.
-% It inputs vlues of a, b and c and compares output with known answer
+% It inputs values of a, b and c and compares output with known answer
 % Returns 1 if the test fails and 0 otherwise.
   if ((makeChocolate(a,b,c)) == ans)
     e=0;
