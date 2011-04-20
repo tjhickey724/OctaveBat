33a156f19896fa7ad668de4dc6eecdf86a5ce9e9
Mehreen Asad <mehreenasad@Mehreen-Asads-MacBook-Pro.local>
2011-04-20 14:43:06 -0400

    Mage changes in test code

--- test_greenticket.m
+++ test_greenticket.m
@@ -1,26 +1,37 @@
-function [ e ] = test_greenticket()
+function e  = test_greenticket()
 %tests greenticket and determines the number of outcomes that fail
+
+%Mehreen Asad(Made changes as shown in front of each test case, Added frpintf 
+%statements which were not there originally, Added a new test case)
+
+
 e=0;
-e=e+runtest(1,1,1,30)
-e=e+runtest(1,1,2,20)
-e=e+runtest(1,2,3,0)
-e=e+runtest(2,1,1,20)
-e=e+runtest(1,2,1,20)
-e=e+runtest(3,2,1,0)
-e=e+runtest(20,200,2,0)
-e=e+runtest(5,05,50,20)
-e=e+runtest(3,3,30,20)
-e=e+runtest(500,500,500,30)
+e=e+runtest(1,1,1,20); % originally was 30, should be 20
+e=e+runtest(1,1,2,10); %originally was 20, should be 10
+e=e+runtest(1,2,3,0);
+e=e+runtest(2,1,1,10); % originally was 20, should be 10
+e=e+runtest(1,2,1,10); % originally was 20, should be 10
+e=e+runtest(3,2,1,0);
+e=e+runtest(20,200,2,0);
+e=e+runtest(5,05,50,10);% originally was 20, should be 10
+e=e+runtest(3,3,30,10); % originally was 20, should be 10
+e=e+runtest(500,500,500,20);% originally was 30, should be 20
+e=e+runtest(4,8,4,10)
+
 end
-    function [e]= runtest(a,b,c,answer)
+
+    function e = runtest(a,b,c,answer)
         %calls in greenticket(a,b,c) and compares the result to the
         %expected answer
         
         result=greenticket(a,b,c);
-        if result==answer
-            e=0;
+       if result==answer
+	 e=0;	 
+fprintf('      greenticket(%d,%d,%d)=%d\n',a,b,c,result);            
+
         else
             e=1;
+fprintf('ERROR:greenricket(%d,%d,%d)->%d <> %d\n', a,b,c,result,answer);
         end
     end
         
