691da2b41f71372b30f69f2042c44b615a3ab553
Mehreen Asad <mehreenasad@Mehreen-Asads-MacBook-Pro.local>
2011-04-20 13:17:03 -0400

    Changed test cases to make sure they are correct and added new test cases

--- test_in3050.m
+++ test_in3050.m
@@ -3,17 +3,23 @@ function e = test_in3050()
 %  the number of tests that fail
 % Author: Iain Crosby
 % Date 2/28/11
+% Mehreen Asad (Changed test cases to show correct result, Added 3 test cases)
 
-	e=0;
-	e= e + runtest(20,40,false);
-	e= e + runtest(35,49,true);
-	e= e + runtest(99,200,false);
-	e= e + runtest(44,33,true);
-	e= e + runtest(9999,8888,false);
-	e= e + runtest(34,8976454,false);
-	e= e + runtest(44,33,true);
-	e= e + runtest(45,32,true);
-	e= e + runtest(30,50, true);
+e=0;
+e= e + runtest(20,40,false);
+e= e + runtest(35,49,false); % this was originally true but should be false
+e= e + runtest(99,200,false);
+e= e + runtest(44,33,false); % this was originally true but should be false
+e= e + runtest(9999,8888,false);
+e= e + runtest(34,8976454,false);
+e= e + runtest(44,33,false); % this was originally true but should be false
+e= e + runtest(45,32,false); % this was originally true but should be false
+e= e + runtest(30,50, false);% this was originally true but should be false
+e= e + runtest(30,41,false);% Added this test case
+e= e + runtest(43,45,true);% Added this test case
+e= e + runtest(37,38,true);% Added this test case
+
+
 
 end
 
