8b8007eb3f31fa96cf06aef30df024833f990f7c
Josh Silverman <joshs.silverman@gmail.com>
2011-04-27 13:46:20 -0400

    added negative numbers to count clumps test; all passing

--- test_countClumps.m
+++ test_countClumps.m
@@ -1,6 +1,10 @@
 function e = test_countClumps()
 #Jeremy Patton
 
+#Tested by: 
+#
+#1. Josh Silverman
+
 e=0;
 e+=runTest([1, 2, 2, 3, 4, 4, 2], 2);
 e+=runTest([1, 1, 2, 1, 1], 2);
@@ -11,6 +15,11 @@ e+=runTest([0, 2, 2, 1, 1, 1, 2, 1, 1, 2, 2], 4);
 e+=runTest([0, 0, 2, 2, 1, 1, 1, 2, 1, 1, 2, 2] , 5);
 e+=runTest([0, 0, 0, 2, 2, 1, 1, 1, 2, 1, 1, 2, 2], 5);
 e+=runTest([], 0);
+e+=runTest([1, 1, 1, 1, 1], 1);
+e+=runTest([0, 2, 3, 2, 1, 4, 1, 5, 1, 2, 1, 6, 1, 2, 2], 1);
+e+=runTest([0, 0, 2, 2, 1, 1, 1, 2, 1, 1, 2] , 4);
+e+=runTest([0, 0, 0, 2, 2, 1, 1, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 5);
+e+=runTest([-1, -1, 2, 2, 1, -1], 2);
 end
 
 function k=runTest(nums, answer)
