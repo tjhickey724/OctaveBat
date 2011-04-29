4465532b0ee7559952ddf36348019d05ca8c5989
Josh Silverman <joshs.silverman@gmail.com>
2011-04-29 16:51:02 -0400

    count7 - added some boundary tests, already had good coverage

--- test_count7.m
+++ test_count7.m
@@ -2,6 +2,10 @@ function e=test_count7()
 % test_count7() runs unit tests on count7(n) and returns the cases that fail
 % author: Varenya Prasad
 % date: 2nd March 2011
+
+% Tested by:
+% Josh Silverman (added some boundary tests, already had good coverage)
+
 e=0;
 e+= runTest(717,2);
 e+= runTest(07,1);
@@ -16,6 +20,10 @@ e+= runTest(70701277,4);
 e+= runTest(777576197,5);
 e+= runTest(99999,0);
 e+= runTest(99799,1);
+e+= runTest(0,0);
+e+= runTest(1283409123840128340128409820498230948209840912840982340982134098213094,0);
+e+= runTest(170792874071872,5);
+e+= runTest(777777777777777,15);
 end
 
 function k = runTest(a,answer)
