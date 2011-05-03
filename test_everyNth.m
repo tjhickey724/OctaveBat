b19142a36e66150a89b63ab8177927c3f7c687b2
Iain Crosby <ifcrosby@gmail.com>
2011-05-03 00:15:12 -0400

    added one test case and added comment line of what I did

--- test_everyNth.m
+++ test_everyNth.m
@@ -2,6 +2,7 @@ function e = test_everyNth()
 % test_everyNth() - runs unit tests on the everyNth function and returns number of tests that fail
 % author: Tim Hickey
 % date: 14 Feb 2011
+  % Iain Crosby, runs fine, added one case to test code 'This is a string'
   e=0;
   e += runTest("Miracle",2,"Mrce");
   e += runTest("abcdefg",2,"aceg");
@@ -10,6 +11,7 @@ function e = test_everyNth()
   e += runTest("Chocolates",3,"Ccas");
   e += runTest("Chocolates",4,"Coe");
   e += runTest("Chocolates",100,"C");
+e += runTest("This is a String", 3, "Tss rg"); %added by icrosby
 end
 
 function k = runTest(s,n,answer)
