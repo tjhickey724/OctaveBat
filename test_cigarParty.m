b68222e99c2b886d1de69083f38313bc9fd34576
Iain Crosby <ifcrosby@gmail.com>
2011-05-02 21:05:09 -0400

    Test runs fine, added bottom two test icrosby

--- test_cigarParty.m
+++ test_cigarParty.m
@@ -17,11 +17,11 @@ function test_cigarParty()
 	e = e + runtest(59,false,true);
 	e = e + runtest(0,true,false);
 	e = e + runtest(39,true,false);
-	e = e + runtest(-50,false,false);
-    
+	e = e + runtest(-50,false,false);    
     e = e + runtest(60.5, true, true);
     e = e + runtest(.5, false, false);
-    
+e = e + runtest(200000,false,false); % Test runs fine, added this case icrosby
+  e = e + runtest(200000,true,true); % and this one icrosby 
     fprintf('\nErrors: %d\n\n', e);
 end
 
