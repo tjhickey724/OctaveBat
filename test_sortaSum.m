f862f29e8034a190a828dd83e8c1b8bc1be3d964
Ritu Pradhan <rituzpradhan@gmail.com>
2011-05-05 01:31:43 -0600

    added test cases

--- test_sortaSum.m
+++ test_sortaSum.m
@@ -7,6 +7,10 @@ function test_sortaSum()
 %modified by Sean Kilachand
 % Corrected syntax
 % Added 2 test cases
+
+%Modified by Ritu Pradhan
+% Added 2 test cases and modified the statement so that errors displays properly.
+
 e = 0;
 e = e + runtest(3,4,7);
 e = e + runtest(9,4,20);
@@ -21,20 +25,22 @@ e = e + runtest(14,5,20);
 e = e + runtest(-10,10,0);
 e = e + runtest(15.5, 2.8, 20);
 
+e = e + runtest(0,0,0);
+e = e + runtest(-3,-3,-6);
+
 fprintf('Failures: %d\n', e);
 end
 
 function e = runtest(a, b, ans)
 % calls sortaSum() on a and b, and comparest it to ans
 % returns 1 if test fails, 0 if it holds
-
-if(sortaSum(a, b) == ans)
+result = sortaSum(a, b);
+if(result == ans)
 	e = 0;
-		fprintf('sortaSum(%d, %d) = %d\n\n', a, b, ans);
+		fprintf('sortaSum(%d, %d) = %d\n\n', a, b, result);
 
 else
 	e = 1;
-	fprintf('Error: sortaSum(%d, %d) != %d\n\n', a, b, ans);
-
+	fprintf('ERROR: sortaSum(%d, %d)->%d<> %d\n',a, b,result,ans);
 end
 end
