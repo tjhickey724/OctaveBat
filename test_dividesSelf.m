2e77de42188c785edc04aef3a0204ec3366eec66
Ritu Pradhan <rituzpradhan@gmail.com>
2011-05-05 01:25:07 -0600

    added new test cases

--- test_dividesSelf.m
+++ test_dividesSelf.m
@@ -3,6 +3,9 @@ function e=test_dividesSelf()
 % author: Varenya Prasad
 % date: 2nd March 2011
 
+% Modified by Ritu Pradhan
+% Added 3 test cases.
+
 e=0;
 e+= runTest(128,1);
 e+= runTest(12,1);
@@ -14,6 +17,10 @@ e+= runTest(42,0);
 e+= runTest(212,1);
 e+= runTest(213,0);
 e+= runTest(162,1);
+
+e+= runTest(0,0);
+e+= runTest(4560,0);
+e+= runTest(122,1);
 end
 
 function k = runTest(a,answer)
