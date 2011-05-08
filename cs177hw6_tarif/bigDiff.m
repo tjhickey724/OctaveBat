a85fd1045d9762e1fcbd4d14ad441dd3b332e3bf
trr <tarifrr@brandeis.edu>
2011-05-08 19:16:09 -0400

    solutions added

---
+++ cs177hw6_tarif/bigDiff.m
@@ -0,0 +1,11 @@
+%Tarif Riyad Rahman
+%MATLAB Code Used
+%Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array. Note: the built-in Math.min(v1, v2) and Math.max  %(v1, v2) methods return the smaller or larger of two values. 
+
+function diff=bigDiff(numArray)
+
+       
+    diff=max(numArray)-min(numArray);
+    
+
+end
