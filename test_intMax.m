ebb49131795f31c16528524c126a27659c97b24a
Liz Li <ezli@brandeis.edu>
2011-05-09 07:50:51 -0400

    added a test case

--- test_intMax.m
+++ test_intMax.m
@@ -19,6 +19,7 @@ e= e + runtest(1,1,1,1);
 e= e + runtest(0,0,0,0);
 e=e+runtest(9/8,21/8,50/5,10);% added by seife kassahun
 e=e+runtest(sqrt(2),sqrt(1),sqrt(5),sqrt(5));% added by seife kassahun
+e= e + runtest(-5,0.3,0,0.3);% added by Liz Li
 
 
 
